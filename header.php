896071cc06baebbc6be4e3f6e3e73cb8b768508f
natashasandy <natashasandy@berkeley.edu>
2015-01-27 11:11:58 -0800

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 
-<h1><a href="index.html"><span class="coursename"><img src="Logo.png" alt="Snap"> </span>Below the Line<img src="Alonzo.gif" alt="Alonzo" align="right" style="position:absolute; right:7.5%"></a><br>Fall 2014</h1>
+<h1><a href="index.html"><span class="coursename"><img src="Logo.png" alt="Snap"> </span>Below the Line<img src="Alonzo.gif" alt="Alonzo" align="right" style="position:absolute; right:7.5%"></a><br>Spring 2015</h1>
 
 	
   <ul class="courseinfo">
       <li>Facilitators: <a href="developers.html#kyle">Kyle Hotchkiss</a>, <a href="developers.html#natasha">Natasha Sandy</a>, and <a href="developers.html#ava">"Ava" Yuan Yuan</a></li>
       <li>Faculty Sponsor: <a href="developers.html#brian">Brian Harvey</a></li>
-      <li> Tuesdays, 6-8 PM in (Insert Location Here)</li>
+      <li> Wednesdays, 7-9 PM in 275 Soda</li>
     </ul>
     <hr/>
     <ul class="navigation">
@@ -13,7 +13,7 @@
       <li><a href="about.html">Course Info</a></li>
       <li><a href="developers.html">Developers</a></li>
       <li><a href="http://snap.berkeley.edu/" target="_blank">Snap!</a></li>
-      <li><a href="https://github.com/cs10/snap" target="_blank">CS10 Snap! GitHub</a></li>
+      <li><a href="https://github.com/ucb-snap/Snap-decal-Sp15" target="_blank">Snap! Below the Line GitHub</a></li>
       <li><a href="https://github.com/jmoenig/Snap--Build-Your-Own-Blocks" target="_blank">Official Snap! GitHub</a></li>
       <li><a href="courseresources.html">Course Resources</a></li>
     </ul>
